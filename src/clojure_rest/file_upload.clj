8aa6d30308cea012de5a8aeb9b9b5b4c53d1e4cc
kenystev <kjestevez.0795@gmail.com>
2016-12-16 23:33:00 -0600

    update profile picture

--- src/clojure_rest/file_upload.clj
+++ src/clojure_rest/file_upload.clj
@@ -2,12 +2,23 @@
       (:use ring.util.response)
       (:require 
       	[clojure.java.jdbc :as sql]
+        [clojure.java.io :refer :all]
       	[clojure.contrib [duck-streams :as ds]]
       	))
 
 (defn upload-file
-  [file]
+  [file namefile]
   (println "Trying to upload a file")
-  (ds/copy (file :tempfile) (ds/file-str "file.out"))
+  (ds/copy (file :tempfile) (ds/file-str namefile))
   (response "File-Upload")
+  )
+
+(defn upload-file-to
+  [file namefile]
+  (println "Trying to upload a file")
+  (let [dir-name (str "db/profilePictures/" namefile)]
+    (make-parents dir-name)
+    (ds/copy (file :tempfile) (ds/file-str dir-name))
+    (response "File-Upload")
+    )
   )
